0da2739c6130f0efe654a34a7e4833523f769e26
Markus Hudobnik <markus.hudobnik@survone.com>
2019-06-25 10:37:52 -0600

    updated

--- components/index.js
+++
@@ -1,6 +0,0 @@
-export default from "./tabs";
-export { Dropdown } from "./dropdown";
-export { Button } from "./button/Button";
-export { Icon } from "./icon";
-export { Card } from "./card/Card";
-export { CardGroup } from "./card/CardGroup";
---
+++ index.js
@@ -0,0 +1,6 @@
+export default from "./components/tabs";
+export { Dropdown } from "./components/dropdown";
+export { Button } from "./components/button/Button";
+export { Icon } from "./components/icon";
+export { Card } from "./components/card/Card";
+export { CardGroup } from "./components/card/CardGroup";
