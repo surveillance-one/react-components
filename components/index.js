ce92480a4d913f48cba15be5db27fe89161fb276
Markus Hudobnik <markus.hudobnik@survone.com>
2019-06-25 08:40:35 -0600

    index changes

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { Tabs } from "./tabs";
+export { Dropdown } from "./dropdown";
+export { Button } from "./button/Button";
+export { Icon } from "./icon";
+export { Card } from "./card/Card";
+export { CardGroup } from "./card/CardGroup";
--- index.js
+++
@@ -1,7 +0,0 @@
-export { Tabs } from "./Tabs";
-export { Results } from "./Results";
-export { Dropdown } from "./Dropdown";
-export { Button } from "./Button/Button";
-export { Icon } from "./Icon";
-export { Card } from "./Card/Card";
-export { CardGroup } from "./Card/CardGroup";
