d20c4168dd82c54c2fc0ed547894b207f9d29a47
Markus Hudobnik <markus.hudobnik@survone.com>
2019-06-25 08:28:11 -0600

    merged dev

---
+++ components/card/index.jsx
@@ -0,0 +1,5 @@
+// "@babel/plugin-proposal-export-default-from",
+
+import Card from "./Card";
+
+export default Card;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-export { default as Button } from "components/button/Button";
+export { Tabs } from "./Tabs";
+export { Results } from "./Results";
+export { Dropdown } from "./Dropdown";
+export { Button } from "./Button/Button";
+export { Icon } from "./Icon";
+export { Card } from "./Card/Card";
+export { CardGroup } from "./Card/CardGroup";
