bdf07c2d6aed1fe979b5c5ba659a8ba39f5e8ba3
Markus Hudobnik <markus.hudobnik@survone.com>
2019-07-17 13:35:07 -0600

    testing npm installs

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-let env;
-if (NODE_ENV === "production") env = require("./dist/index.js");
-else env = require("./src/lib/index.js");
 
-module.exports = env;
+module.exports = require("./dist/index.js");
