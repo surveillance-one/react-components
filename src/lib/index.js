f2cc2637844c001d1dd8ed7b93059693a5651a4e
Markus Hudobnik <markus.hudobnik@survone.com>
2019-08-21 09:24:12 -0600

    Normalize

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,22 @@
-export { Icon } from "./icon";
+import { normalize, Normalize } from "./normalize";
 
-export { TabGroup, Tab } from "./tabs";
-export { Dropdown, Item } from "./dropdown";
-export { Card, CardGroup } from "./card";
-export { Button, ButtonGroup } from "./button";
+import { Icon } from "./icon";
+import { TabGroup, Tab } from "./tabs";
+import { Dropdown, Item } from "./dropdown";
+import { Card, CardGroup } from "./card";
+import { Button, ButtonGroup } from "./button";
 
+
+export {
+	normalize,
+	Normalize,
+	Icon,
+	TabGroup,
+	Tab,
+	Dropdown,
+	Item,
+	Card,
+	CardGroup,
+	Button,
+	ButtonGroup
+};
